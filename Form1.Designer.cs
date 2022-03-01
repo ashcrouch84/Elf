8fbf77888e2e92b59b2b3f9331343c4d219dbc80
ashcrouch84 <73351045+ashcrouch84@users.noreply.github.com>
2022-03-01 14:55:28 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿
+namespace Elf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblF4 = new System.Windows.Forms.Label();
+            this.lblF3 = new System.Windows.Forms.Label();
+            this.lblF2 = new System.Windows.Forms.Label();
+            this.lblF1 = new System.Windows.Forms.Label();
+            this.lblE4 = new System.Windows.Forms.Label();
+            this.lblE3 = new System.Windows.Forms.Label();
+            this.lblE2 = new System.Windows.Forms.Label();
+            this.lblE1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.cmdSave = new System.Windows.Forms.Button();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboMon = new System.Windows.Forms.ComboBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.cmdMon = new System.Windows.Forms.Button();
+            this.txtMon = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.ReadFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1049, 614);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Green;
+            this.tabPage1.Controls.Add(this.lblF4);
+            this.tabPage1.Controls.Add(this.lblF3);
+            this.tabPage1.Controls.Add(this.lblF2);
+            this.tabPage1.Controls.Add(this.lblF1);
+            this.tabPage1.Controls.Add(this.lblE4);
+            this.tabPage1.Controls.Add(this.lblE3);
+            this.tabPage1.Controls.Add(this.lblE2);
+            this.tabPage1.Controls.Add(this.lblE1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(1041, 588);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // lblF4
+            // 
+            this.lblF4.AutoSize = true;
+            this.lblF4.Font = new System.Drawing.Font("Monotype Corsiva", 90F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblF4.Location = new System.Drawing.Point(585, 464);
+            this.lblF4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblF4.Name = "lblF4";
+            this.lblF4.Size = new System.Drawing.Size(348, 162);
+            this.lblF4.TabIndex = 7;
+            this.lblF4.Text = "label5";
+            // 
+            // lblF3
+            // 
+            this.lblF3.AutoSize = true;
+            this.lblF3.Font = new System.Drawing.Font("Monotype Corsiva", 90F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblF3.Location = new System.Drawing.Point(577, 304);
+            this.lblF3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblF3.Name = "lblF3";
+            this.lblF3.Size = new System.Drawing.Size(348, 162);
+            this.lblF3.TabIndex = 6;
+            this.lblF3.Text = "label6";
+            // 
+            // lblF2
+            // 
+            this.lblF2.AutoSize = true;
+            this.lblF2.Font = new System.Drawing.Font("Monotype Corsiva", 90F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblF2.Location = new System.Drawing.Point(574, 162);
+            this.lblF2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblF2.Name = "lblF2";
+            this.lblF2.Size = new System.Drawing.Size(348, 162);
+            this.lblF2.TabIndex = 5;
+            this.lblF2.Text = "label7";
+            // 
+            // lblF1
+            // 
+            this.lblF1.AutoSize = true;
+            this.lblF1.Font = new System.Drawing.Font("Monotype Corsiva", 90F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblF1.Location = new System.Drawing.Point(574, 9);
+            this.lblF1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblF1.Name = "lblF1";
+            this.lblF1.Size = new System.Drawing.Size(348, 162);
+            this.lblF1.TabIndex = 4;
+            this.lblF1.Text = "label8";
+            // 
+            // lblE4
+            // 
+            this.lblE4.AutoSize = true;
+            this.lblE4.Font = new System.Drawing.Font("Monotype Corsiva", 90F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblE4.Location = new System.Drawing.Point(27, 464);
+            this.lblE4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblE4.Name = "lblE4";
+            this.lblE4.Size = new System.Drawing.Size(348, 162);
+            this.lblE4.TabIndex = 3;
+            this.lblE4.Text = "label3";
+            // 
+            // lblE3
+            // 
+            this.lblE3.AutoSize = true;
+            this.lblE3.Font = new System.Drawing.Font("Monotype Corsiva", 90F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblE3.Location = new System.Drawing.Point(19, 304);
+            this.lblE3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblE3.Name = "lblE3";
+            this.lblE3.Size = new System.Drawing.Size(348, 162);
+            this.lblE3.TabIndex = 2;
+            this.lblE3.Text = "label4";
+            // 
+            // lblE2
+            // 
+            this.lblE2.AutoSize = true;
+            this.lblE2.Font = new System.Drawing.Font("Monotype Corsiva", 90F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblE2.Location = new System.Drawing.Point(16, 162);
+            this.lblE2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblE2.Name = "lblE2";
+            this.lblE2.Size = new System.Drawing.Size(348, 162);
+            this.lblE2.TabIndex = 1;
+            this.lblE2.Text = "label2";
+            // 
+            // lblE1
+            // 
+            this.lblE1.AutoSize = true;
+            this.lblE1.Font = new System.Drawing.Font("Monotype Corsiva", 90F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblE1.Location = new System.Drawing.Point(16, 9);
+            this.lblE1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblE1.Name = "lblE1";
+            this.lblE1.Size = new System.Drawing.Size(348, 162);
+            this.lblE1.TabIndex = 0;
+            this.lblE1.Text = "label1";
+            this.lblE1.Click += new System.EventHandler(this.lblE1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.cmdSave);
+            this.tabPage2.Controls.Add(this.txtInterval);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.cboMon);
+            this.tabPage2.Controls.Add(this.groupBox6);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(1104, 588);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // cmdSave
+            // 
+            this.cmdSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSave.Location = new System.Drawing.Point(278, 187);
+            this.cmdSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmdSave.Name = "cmdSave";
+            this.cmdSave.Size = new System.Drawing.Size(109, 27);
+            this.cmdSave.TabIndex = 16;
+            this.cmdSave.Text = "Save";
+            this.cmdSave.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.cmdSave.UseVisualStyleBackColor = true;
+            this.cmdSave.Click += new System.EventHandler(this.cmdSave_Click);
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInterval.Location = new System.Drawing.Point(160, 187);
+            this.txtInterval.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(98, 34);
+            this.txtInterval.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 184);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 36);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Interval";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 126);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 36);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Side";
+            // 
+            // cboMon
+            // 
+            this.cboMon.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboMon.FormattingEnabled = true;
+            this.cboMon.Location = new System.Drawing.Point(145, 127);
+            this.cboMon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboMon.Name = "cboMon";
+            this.cboMon.Size = new System.Drawing.Size(68, 38);
+            this.cboMon.TabIndex = 12;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.cmdMon);
+            this.groupBox6.Controls.Add(this.txtMon);
+            this.groupBox6.Controls.Add(this.label11);
+            this.groupBox6.Location = new System.Drawing.Point(15, 21);
+            this.groupBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox6.Size = new System.Drawing.Size(512, 81);
+            this.groupBox6.TabIndex = 8;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Save Information Location";
+            // 
+            // cmdMon
+            // 
+            this.cmdMon.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdMon.Location = new System.Drawing.Point(361, 33);
+            this.cmdMon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmdMon.Name = "cmdMon";
+            this.cmdMon.Size = new System.Drawing.Size(109, 27);
+            this.cmdMon.TabIndex = 14;
+            this.cmdMon.Text = "Browse";
+            this.cmdMon.UseVisualStyleBackColor = true;
+            this.cmdMon.Click += new System.EventHandler(this.cmdBrowseSave_Click);
+            // 
+            // txtMon
+            // 
+            this.txtMon.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMon.Location = new System.Drawing.Point(212, 33);
+            this.txtMon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMon.Name = "txtMon";
+            this.txtMon.Size = new System.Drawing.Size(133, 34);
+            this.txtMon.TabIndex = 13;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(43, 36);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(191, 29);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Monitor Location";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1049, 614);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label lblF4;
+        private System.Windows.Forms.Label lblF3;
+        private System.Windows.Forms.Label lblF2;
+        private System.Windows.Forms.Label lblF1;
+        private System.Windows.Forms.Label lblE4;
+        private System.Windows.Forms.Label lblE3;
+        private System.Windows.Forms.Label lblE2;
+        private System.Windows.Forms.Label lblE1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboMon;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Button cmdMon;
+        private System.Windows.Forms.TextBox txtMon;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.FolderBrowserDialog ReadFolderBrowser;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button cmdSave;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace Elf
+{
+    public partial class Form1 : Form
+    {
+        int interror;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdBrowseSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(txtMon.Text)) ReadFolderBrowser.SelectedPath = txtMon.Text;
+                if (ReadFolderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    txtMon.Text = ReadFolderBrowser.SelectedPath;
+                    Properties.Settings.Default.savedLoc = txtMon.Text;
+                    Properties.Settings.Default.Save();
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var list = new List<string>();
+            string strread = "";
+
+
+            try
+            {
+                strread = Properties.Settings.Default.savedLoc.ToString();
+                strread = strread + "\\" + cboMon.SelectedItem.ToString() + ".txt";
+                // MessageBox.Show(strread.ToString());
+                var fileStream = new FileStream(strread, FileMode.Open, System.IO.FileAccess.Read);
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    string line;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        list.Add(line);
+                    }
+                }
+                fileStream.Close();
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                if (list.Count < 0)
+                { }
+                else
+                {
+                    lblE1.Text = list[0].ToString();
+                    lblE2.Text = list[1].ToString();
+                    lblE3.Text = list[2].ToString();
+                    lblE4.Text = list[3].ToString();
+                    lblF1.Text = list[4].ToString();
+                    lblF2.Text = list[5].ToString();
+                    lblF3.Text = list[6].ToString();
+                    lblF4.Text = list[7].ToString();
+                }
+            }
+            catch
+            { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtMon.Text = Properties.Settings.Default.savedLoc.ToString();
+            cboMon.Items.Add("Red");
+            cboMon.Items.Add("Blue");
+            cboMon.Text = Properties.Settings.Default.savedID.ToString();
+            interror = 0;
+            txtInterval.Text = Properties.Settings.Default.savedInterval.ToString();
+            timer1.Interval = Properties.Settings.Default.savedInterval;
+            timer1.Enabled = true;
+
+        }
+
+        private void cmdSave_Click(object sender, EventArgs e)
+        {
+            int i = Properties.Settings.Default.savedInterval;
+            timer1.Enabled = false;
+            try
+            {
+                Properties.Settings.Default.savedInterval = Int32.Parse(txtInterval.Text);
+                Properties.Settings.Default.Save();
+                timer1.Interval = Int32.Parse(txtInterval.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Interval needs to be a valid number, please try again", "Interval Error");
+                timer1.Interval = Properties.Settings.Default.savedInterval;
+            }
+            timer1.Enabled = true;
+        }
+
+        private void lblE1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
